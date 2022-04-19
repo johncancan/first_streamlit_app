0daa0801bf1e6eb923d2dd7a0dc00ebbc44bde77
Johncancan <johncancansnowflake@gmail.com>
2022-04-20 01:32:24 +1200

    Update streamlit_app.py
    changed Mon's to Parents again
    added header
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Mon\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+Streamlit.text('Hard-Boild Free-Range Egg')
